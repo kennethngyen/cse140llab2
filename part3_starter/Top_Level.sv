a4be443124ef2462c487df4a23da40fd58b08032
panda2k <thelowkeypanda@gmail.com>
2024-05-08 23:40:24 -0700

    mfkjdaskfjdaskj

--- part3_starter/Top_Level.sv
+++ part3_starter/Top_Level.sv
@@ -1,13 +1,15 @@
 // CSE140L  
 // see Structural Diagram in Lab2 assignment writeup
 // fill in missing connections and parameters
-module Top_Level #(parameter NS=60, NH=24, ND=7)(
+module Top_Level #(parameter NS=60, NH=24, ND=7, NM=12)(
   input Reset,
         Timeset, 	  // manual buttons
         Alarmset,	  //	(five total)
 		Minadv,
 		Hrsadv,
 		Dayadv,
+        Datadv
+        Monadv,
 		Alarmon,
 		Pulse,		  // digital clock, assume 1 cycle/sec.
 // 6 decimal digit display (7 segment)
@@ -15,13 +17,14 @@ module Top_Level #(parameter NS=60, NH=24, ND=7)(
                M1disp, M0disp, 
                H1disp, H0disp,
                D0disp,   // for part 2
+               T1disp, T0disp,
+               N1disp, N0disp,
   output logic Buzz);	           // alarm sounds
 // internal connections (may need more)
-  logic[6:0] TSec, TMin, THrs, TDays, // clock/time 
+  logic[6:0] TSec, TMin, THrs, TDays, TDate, TMonth, DateMod, // clock/time 
              AMin, AHrs, ADays;		   // alarm setting
-  logic S_max, M_max, H_max, 	   // "carry out" from sec -> min, min -> hrs, hrs -> days
+  logic S_max, M_max, H_max, Date_max, 	   // "carry out" from sec -> min, min -> hrs, hrs -> days
         TMen, THen, AMen, AHen; 
-
 // (almost) free-running seconds counter	-- be sure to set modulus inputs on ct_mod_N modules
   ct_mod_N  Sct(
 // input ports
@@ -53,7 +56,31 @@ module Top_Level #(parameter NS=60, NH=24, ND=7)(
     .clk(Pulse), .rst(Reset), .en(H_max && M_max && S_max || Timeset && Dayadv), .modulus(ND),
 // output ports
     .ct_out(TDays), .ct_max());
-  
+
+    always_comb begin 
+        if (TMonth == 0 || TMonth == 2 || TMonth == 4 || TMonth == 6 || TMonth == 7 || TMonth == 9 || TMonth == 11) begin 
+            DateMod = 31;
+        end else if (TMonth == 1) begin 
+            DateMod = 29;
+        end else begin 
+            DateMod = 30;
+        end
+    end
+
+  ct_mod_N  Tct(
+// input ports
+    .clk(Pulse), .rst(Reset), .en(H_max && M_max && S_max || Timeset && Datadv), .modulus(DateMod),
+// output ports
+    .ct_out(TDate), .ct_max(Date_max));
+
+  ct_mod_N  Nct(
+// input ports
+    .clk(Pulse), .rst(Reset), .en(Date_max && H_max && M_max && S_max || Timeset && Monadv), .modulus(NM),
+// output ports
+    .ct_out(TMonth), .ct_max());
+
+    // REGISTERS
+
 // alarm set registers -- either hold or advance 1/sec while being set
   ct_mod_N Mreg(
 // input ports
@@ -96,6 +123,18 @@ module Top_Level #(parameter NS=60, NH=24, ND=7)(
     .bin_in    (Alarmset ? ADays : TDays),
     .Segment1  (),
     .Segment0  (D0disp)
+	);
+
+  lcd_int Dadisp(
+    .bin_in    (TDate + 1),
+    .Segment1  (T1disp),
+    .Segment0  (T0disp)
+	);
+
+  lcd_int Modisp(
+    .bin_in    (TMonth + 1),
+    .Segment1  (N1disp),
+    .Segment0  (N0disp)
 	);
 
 // buzz off :)	  make the connections
